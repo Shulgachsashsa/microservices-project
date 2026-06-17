7b005fd015c45113f3e1b3642cadf87b2edc6332
Shulgachsashsa <sanyashulgach@gmail.com>
2026-06-17 21:43:11 +0300

    dsdsdsd

--- api-gateway/src/main/java/demo/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/demo/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package demo.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
--- auth-service/src/main/java/demo/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/demo/authservice/AuthServiceApplication.java
@@ -2,7 +2,9 @@ package demo.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
+@ControllerAdvice
 @SpringBootApplication
 public class AuthServiceApplication {
 
